c0bfa000729925f94aa28baca264e0c724533c0d
Rakshith Ramagiri <rakshithramagiri96@gmail.com>
2019-03-17 17:35:46 +0530

    update : finished model definition

--- model.py
+++ model.py
@@ -3,9 +3,24 @@ import torch.nn as nn
 
 
 class DQ_NETWORK(nn.Module):
-    def __init__(self):
+    def __init__(self, state_size, action_size, seed, hidden=512):
         super(DQ_NETWORK, self).__init__()
-        pass
 
-    def forward(self):
-        pass
+        self.state_size = state_size
+        self.action_size = action_size
+        self.hidden_size = hidden_size
+        torch.manual_seed(seed)
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.state_size, self.hidden_size),
+            nn.ReLU(),
+
+            nn.Linear(self.hidden_size, self.hidden_size),
+            nn.ReLU(),
+
+            nn.Linear(self.hidden_size, self.action_size),
+        )
+
+    def forward(self, state):
+        output = self.fc(state)
+        return output
