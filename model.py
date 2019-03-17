228524711188fde5c123b895abbbb765908403a5
Rakshith Ramagiri <rakshithramagiri96@gmail.com>
2019-03-17 17:24:37 +0530

    add : added dq network definition file

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class DQ_NETWORK(nn.Module):
+    def __init__(self):
+        super(DQ_NETWORK, self).__init__()
+        pass
+
+    def forward(self):
+        pass
